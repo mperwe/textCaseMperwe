3486c24aa32fd50bfedd13a12693a9c35b148831
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-02 10:36:43 +0200

    Add package files

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function to convert a string to sentence case
+function toSentenceCase(inputString) {
+    if (typeof inputString !== 'string') {
+        throw new Error('string in put');
+    }
+    // input string - to words
+    const words = inputString.toLowerCase().split(' ');
+    // Enabling santence -casing
+    const sentenceCaseWords = words.map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    // .Join, to join words together and form the sentence case string
+    const sentenceCaseString = sentenceCaseWords.join(' ');
+
+    return sentenceCaseString;
+}
+
+// Export the function as part of your library
+module.exports = { toSentenceCase };
